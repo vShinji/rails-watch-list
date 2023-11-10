68df7d7f66c36e7901609f72ea27fb07e0e8b185
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-11-10 18:42:58 +0900

    Image working

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :image
 
   validates :name, uniqueness: true, presence: true
 end
