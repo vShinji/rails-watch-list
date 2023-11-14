3292f98265cb8f95b9db49c9d68b7db3d0a582de
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-11-14 22:40:17 +0900

    XD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  resources :lists, only: %i[index show new create destroy] do
+  resources :lists, only: %i[index show new create ] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: :destroy
