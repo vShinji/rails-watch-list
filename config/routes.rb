39b0338c85916e46546fb020c30a9e1786d2b537
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-11-14 22:36:14 +0900

    Root changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: "lists#index"
 
 
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
+  root to: "lists#index"
 end
